be4124acaefecbaf35eaea716b5398e4ecb4d3a7
roees-rnd <roees@rnd-hub.com>
2020-05-04 15:08:19 +0300

    changed to implementation to class

--- buildGraph.py
+++ buildGraph.py
@@ -5,63 +5,54 @@ import rosparam
 import rospy
 from geometry_msgs.msg import PoseStamped, Pose, PoseArray
 from visualization_msgs.msg import MarkerArray, Marker
+from nav_msgs.msg import OccupancyGrid
 from geometry_msgs import msg as geom_msg
 import numpy as np
 
 
-ndb = net_db.net_db()
+
 # ndb.add_node((0, 0.1), False)
 # ndb.add_node((2, 0.1), True)
+# pos, is_door = ndb.get_all_nodes()
 
-pos, is_door = ndb.get_all_nodes()
+class buildGraph:
+    def __init__(self):
+        rospy.Subscriber('/slam_out_pose', PoseStamped, self.updatePos)
+        self.ndb = net_db.net_db()
+        self.pubNodes = rospy.Publisher('/pubNodes', Marker, queue_size=10)
 
-
-def updatePos(msg):
-    global ndb
-    x = msg.pose.position.x
-    y = msg.pose.position.y
-    ndb.add_node((x, y), False)
-
-
-def publishGraph(pubNodes, pub_ns='net', stamp=None, frame_id='world', size=0.03, numLmks=0):
-    global ndb
-    marker = Marker(type=Marker.LINE_LIST, ns=pub_ns, action=Marker.ADD)
-    marker.header.frame_id = frame_id
-    marker.header.stamp = stamp if stamp is not None else rospy.Time.now()
-    marker.scale.x = size
-    marker.scale.y = size
-    marker.color.b = 0.3
-    marker.color.a = 1.0
-    marker.color.g = 0.7
-    marker.pose.position = geom_msg.Point(0, 0, 0)
-    marker.pose.orientation = geom_msg.Quaternion(0, 0, 0, 1)
-
-    edg = ndb.get_all_edges()
-    marker.points = []
-    for e in edg:
-        marker.points.extend([geom_msg.Point(e[0][0], e[0][1], 0),
-                              geom_msg.Point(e[1][0], e[1][1], 0)])
-
-    marker.lifetime = rospy.Duration(1)
-    pubNodes.publish(marker)    
+    def updatePos(self, msg):
+        x = msg.pose.position.x
+        y = msg.pose.position.y
+        self.ndb.add_node((x, y))
+        self.publishGraph(frame_id='map')
 
 
-# def addFrontier(msg):
-subPose = rospy.Subscriber('/slam_out_pose', PoseStamped, updatePos)
-# /localization/out/pose
-# subFrontiers = rospy.Subscriber('/pathpl PoseArray, addFrontier)
-# 
+    def publishGraph(self, pub_ns='net', stamp=None, frame_id='world', size=0.03, numLmks=0):
+        marker = Marker(type=Marker.LINE_LIST, ns=pub_ns, action=Marker.ADD)
+        marker.header.frame_id = frame_id
+        marker.header.stamp = stamp if stamp is not None else rospy.Time.now()
+        marker.scale.x = size
+        marker.scale.y = size
+        marker.color.b = 0.3
+        marker.color.a = 1.0
+        marker.color.g = 0.7
+        marker.pose.position = geom_msg.Point(0, 0, 0)
+        marker.pose.orientation = geom_msg.Quaternion(0, 0, 0, 1)
 
-pubNodes = rospy.Publisher('/pubNodes', Marker, queue_size=10)
+        edg = self.ndb.get_all_edges()
+        marker.points = []
+        for e in edg:
+            marker.points.extend([geom_msg.Point(e[0][0], e[0][1], 0),
+                                geom_msg.Point(e[1][0], e[1][1], 0)])
 
-rospy.init_node("buildGraph", anonymous=True)
+        marker.lifetime = rospy.Duration(1)
+        self.pubNodes.publish(marker)    
 
-rate = rospy.Rate(1)
-while not rospy.is_shutdown():
-    publishGraph(pubNodes, frame_id='map')
-    rate.sleep()
-
-# rospy.spin()
 
 if __name__ == "__main__":
+    import buildGraph
+    rospy.init_node("buildGraph", anonymous=True)
+    bg = buildGraph.buildGraph()
+    rospy.spin()
     print("buildGraph.py run as main")
