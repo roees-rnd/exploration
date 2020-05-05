1c8f59ef9b8b1ed231c1104fe176c7085d0c67ec
inbalw-rnd <inbalw@rnd-hub.com>
2020-05-05 13:05:42 +0300

    transpose added

--- MapInfo.py
+++ MapInfo.py
@@ -1,5 +1,7 @@
+
 import numpy as np
 import copy
+
 
 class MapInfo:
     def __init__(self, width, height, res, x, y, z):
@@ -9,17 +11,18 @@ class MapInfo:
         self.map_delta_x = -x
         self.map_delta_y = -y
         self.map_delta_z = -z
-        self.map= None
+        self.map = None
 
     def set_map(self, map_data):
-        self.map = np.reshape(copy.deepcopy(map_data), (self.map_sizeX,self.map_sizeY))
+        self.map = np.reshape(copy.deepcopy(map_data),
+                              (self.map_sizeY, self.map_sizeX)).T
 
     def xy_to_ij(self, x, y):
-        i = int(np.floor((x + self.map_delta_x) / self.map_resolution))
-        j = int(np.floor((y + self.map_delta_y)/ self.map_resolution))
+        i = int(np.floor((y + self.map_delta_y) / self.map_resolution))
+        j = int(np.floor((x + self.map_delta_x) / self.map_resolution))
         return i, j
 
     def ij_to_xy(self, i, j):
-        x =  i*self.map_resolution - self.map_delta_x
-        y = j*self.map_resolution - self.map_delta_y
+        x = j*self.map_resolution - self.map_delta_x
+        y = i*self.map_resolution - self.map_delta_y
         return x, y
