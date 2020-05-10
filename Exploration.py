3c9a12f9935b69c13559a1878100cd3232af542e
inbalw-rnd <inbalw@rnd-hub.com>
2020-05-10 08:46:36 +0300

    small chenages to exploration

--- Exploration.py
+++ Exploration.py
@@ -1,4 +1,5 @@
 
+#!/usr/bin/env python
 import copy
 import rospy
 from nav_msgs.msg import OccupancyGrid
@@ -7,48 +8,65 @@ from MapInfo import MapInfo
 from geometry_msgs.msg import PoseArray, PoseStamped, WrenchStamped, Pose
 import time
 import numpy as np
-import cv2
+# import cv2
 from Frontiers import FrontierClass
+#import buildGraph
+from ChoseExplorationPoint import ChoseExplorationPointClass
 
+# import sys
+# sys.path.append("/home/roee/catkin_ws/src/exploration/env/lib/python3.6/site-packages")
 
 class ExplorationCalss:
     def __init__(self):
         self.DEBUG_FLAG = False
-
+        rospy.Subscriber("/map", OccupancyGrid, callback=self.do_step)
         # publishers
-        self.PubFrontier = rospy.Publisher('/path_planner/frontiers', PoseArray, queue_size=1)  # list of frontiers to publish
+        self.PubFrontier = rospy.Publisher(
+            '/path_planner/frontiers', PoseArray, queue_size=1)  # list of frontiers to publish
         # PubVer.publish(version)
         # rospy.set_param('/version/path_planner', version)
-
 
         # subdcribers
         # rospy.Subscriber('/localization/out/pose', PoseStamped, self.save_self_pos) # saves self.pos
         # rospy.Subscriber('/mavros/local_position/pose',
-        # 					PoseStamped, self.save_self_pos)  # saves self.pos
-        rospy.Subscriber("/map", OccupancyGrid, callback=self.do_step)
+        #                     PoseStamped, self.save_self_pos)  # saves self.pos
 
         self.frontiers = FrontierClass()
         self.current_frontiers = []
         self.route_to_point = []
         #self.graph = Graph()
-        #self.chose_mission = ChoseExplorationPointClass()
+        self.chose_mission = ChoseExplorationPointClass()
+        #self.buildGraph = buildGraph.buildGraph()
+
+        # rospy.Subscriber('/move_base_simple/goal', PoseStamped, self.route_from_exp)
+        
+
 
         self.map = None
+
+
+    def route_from_exp(self, msg):
+        xy = (msg.pose.position.x, msg.pose.position.y)
+        pose_array = self.buildGraph.getPoseArrayToTarget_as_poseArray(xy, vis=True)
 
     def saveMap(self, msg):
         if self.map is None:
             self.map = MapInfo(msg.info.width, msg.info.height, msg.info.resolution,
                                msg.info.origin.position.x, msg.info.origin.position.y, msg.info.origin.position.z)
         self.map.set_map(msg.data)
+        #self.buildGraph.ndb.map_info = self.map
 
-
+    def map(self, msg):
+        print("WTF")
 
     def do_step(self, mapData):
         self.saveMap(mapData)
         self.current_frontiers = self.frontiers.do_step(self.map)
-        #self.publish_frontiers(self.current_frontiers)
+        self.publish_frontiers(self.current_frontiers)
         nodes_to_add = self.frontiers.new_frontiers
-        nodes_to_remove = self.frontiers.irrelevant_frontiers
+        # for n in self.current_frontiers:  # nodes_to_add:
+        #     self.buildGraph.ndb.add_node_map((n[0], n[1]))
+        # nodes_to_remove = self.frontiers.irrelevant_frontiers
         #self.graph.update(self.map, nodes_to_add, nodes_to_remove)
         #currentTarget = self.chose_mission.do_step(self.graph, currentPos, self.current_frontiers)
         #path = self.graph.route(currentPos, currentTarget)
@@ -59,28 +77,32 @@ class ExplorationCalss:
         return path
 
     def route_to_best_frontier(self):
-        currentTarget = self.chose_mission.do_step(self.graph, current_frontiers)
+        currentTarget, path = self.chose_mission.do_step(self.graph, current_frontiers)
         self.route_to_point = currentTarget
-        path = self.graph.route(currentTarget)
         return path
 
+    def get_covrage(self):
+        if self.map is None:
+            return 0
+
+        size_of_world = self.map.map_sizeX * self.map.map_sizeX
 
     def publish_frontiers(self, frontiers_list):
-		msgOut = PoseArray()
-		msgOut.header.frame_id = 'map'
-		msgOut.header.stamp = rospy.Time.now()
-		pose = Pose()
-		for i in range(len(frontiers_list)):
-			pose.position.x = frontiers_list[i][0]
-			pose.position.y = frontiers_list[i][1]
-			pose.position.z = 0.7
-			pose.orientation.w = 1
-			msgOut.poses.append(copy.deepcopy(pose))
-		self.PubFrontier.publish(msgOut)
-
+        msgOut = PoseArray()
+        msgOut.header.frame_id = 'map'
+        msgOut.header.stamp = rospy.Time.now()
+        pose = Pose()
+        for i in range(len(frontiers_list)):
+            pose.position.x = frontiers_list[i][0]
+            pose.position.y = frontiers_list[i][1]
+            pose.position.z = 0.7
+            pose.orientation.w = 1
+            msgOut.poses.append(copy.deepcopy(pose))
+        self.PubFrontier.publish(msgOut)
 
 
 if __name__ == "__main__":
+    print("in main")
     rospy.init_node('ExplorationCalssNode')
     exploration = ExplorationCalss()
     rospy.spin()
